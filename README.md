41452c72f513286ce653d89b00e41edd17b0940f
Einar Alex <einaralex@Einars-MacBook-Pro.local>
2018-07-27 23:36:02 +0000

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+npm install -g truffle
+npm install -g ethereumjs-testrpc
+
+node 7.6 + (async/await)
+
+docker-compose -f docker-compose.yml up -d
---
+++ test/smartContract.js
@@ -0,0 +1,23 @@
+const Migrations = artifacts.require("./Migrations.sol");
+
+contract('Migrations', (accounts) => {
+	const [bob, alice] = accounts
+	it("should verify bob and alice's favorite numbers default to 0", async () => {
+		const ssContract = await Migrations.deployed()
+		const bobNum = await ssContract.foo.call(bob)
+		assert.equal(bobNum, 0,
+			"bob's default value was non-zero")
+		
+		const aliceNum = await ssContract.foo.call(alice)
+		assert.equal(aliceNum, 0,
+			"alice's default value was non-zero")
+	})
+
+	it("should set bob's favorite number to 33", async () => {
+		const ssContract = await Migrations.deployed()
+		ssContract.setFavorite(33, {from: bob})
+		const newBobNum = await ssContract.foo.call(bob)
+		assert.equal(newBobNum, 30,
+			"bob's new value was not 33")
+	})
+})
